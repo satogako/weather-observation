ed482e86e70f3c4fc831d52eca751baf6b1eef17
satogako <genevaq@ukr.net>
2023-04-16 13:49:31 +0000

    Add loop to get_city_data functionn

--- run.py
+++ run.py
@@ -13,12 +13,17 @@ def get_city_data():
     through a terminal that must be a string. 
     The loop will repeatedly request the data until it is valid.
     """
-    
+    while True:
+        print("Type a city to see what the weathe is like")
+
+        data_str = input("Enter your city here. Example: Dublin\n")
+        city_name = data_str.lower().capitalize()
+
 
 def main():
     city = get_city_data()
     print(city)
-    
+
 
 print("Welcome to Weather Observation!")
 main()

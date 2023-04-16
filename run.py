dc34a5f7e5e3137c8eac2cac38b974783c9de2fb
satogako <genevaq@ukr.net>
2023-04-16 13:46:24 +0000

    Create functions get_city_data and main

--- run.py
+++ run.py
@@ -1,5 +1,24 @@
 from pyowm import OWM
 from prettytable import PrettyTable
 
+
 OMW_KEY = OWM('6527f0e6cac68cd70fb43502cb0948d7')
 MGR = OMW_KEY.weather_manager()
+
+
+def get_city_data():
+    """
+    Get the name of the city entered by the user.
+    Run a while loop to collect a valid string of data from the user
+    through a terminal that must be a string. 
+    The loop will repeatedly request the data until it is valid.
+    """
+    
+
+def main():
+    city = get_city_data()
+    print(city)
+    
+
+print("Welcome to Weather Observation!")
+main()

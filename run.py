7b11b2d8d1e275d25544751dfc73081ff592f6b2
satogako <genevaq@ukr.net>
2023-04-19 12:01:54 +0000

    Add requirements for deployment

--- run.py
+++ run.py
@@ -19,9 +19,9 @@ def get_city_data():
     The loop will repeatedly request the data until it is valid.
     """
     while True:
-        print("Type a city to see what the weathe is like")
+        print('Type a city to see what the weathe is like')
 
-        data_str = input("Enter your city here. Example: Dublin\n")
+        data_str = input('Enter your city here. Example: Dublin\n')
         city_name = data_str.lower().capitalize()
 
         if validate_data(city_name):
@@ -31,15 +31,15 @@ def get_city_data():
 
 
 def validate_data(value):
-    """
+    '''
     Inside the attempt, it checks whether the value is an empty string and
     whether the value consists entirely of letters. Raises ValueError if
     the value is an empty string and does not consist of letters.
     Throws an Exception when the library does not find the city.
-    """
+    '''
     try:
         if value.isalpha() is False:
-            if value == "":
+            if value == '':
                 raise ValueError(
                     "You did't enter any data"
                 )
